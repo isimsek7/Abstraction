dd0a02d6a9f1b30e73de0b2a121c71b4784c7e40
isimsek7 <isimsekmusic@gmail.com>
2024-08-08 18:53:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Abstraction;
+YazilimGelistirme yazilimci1 = new YazilimGelistirme();
+yazilimci1._ad = "Muhsin";
+yazilimci1._soyAd = "Tahsin";
+yazilimci1._departman = "IT";
+
+SatisTemsilcisi satis1 = new SatisTemsilcisi();
+satis1._ad = "Nafiz";
+satis1._soyAd = "Mafiz";
+satis1._departman = "Satis";
+
+ProjeYoneticisi yonetici1 = new ProjeYoneticisi();
+yonetici1._ad = "Vunay";
+yonetici1._soyAd = "Munay";
+yonetici1._departman = "Yonetim";
+
+yazilimci1.Gorev();
+satis1.Gorev();
+yonetici1.Gorev();
---
+++ Staff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Abstraction
+{
+	public abstract class Calisan
+	{
+		public Calisan()
+		{
+		}
+		public string _ad;
+		public string _soyAd;
+		public string _departman;
+		public string _pozisyon;
+
+		public virtual void Gorev()
+		{
+		}
+	}
+	public class ProjeYoneticisi:Calisan
+	{
+        public override void Gorev()
+        {
+			Console.WriteLine("Proje Yoneticisi olarak calisiyorum");
+        }
+    }
+	public class YazilimGelistirme:Calisan
+	{
+        public override void Gorev()
+        {
+            Console.WriteLine("Yazilim gelistiricisi olarak calisiyorum");
+        }
+    }
+    public class SatisTemsilcisi : Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine("Satis temsilcisi olarak calisiyorum");
+        }
+    }
+}
+
